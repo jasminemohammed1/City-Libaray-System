987afb5339a4bfa35854a304920e91efd4099263
jasmine <jasminemohamed174005@gmail.com>
2026-03-11 22:19:14 +0200

    Add more extensions methods

--- CityLibaraySystem/Extensions/StringExtensions.cs
+++ CityLibaraySystem/Extensions/StringExtensions.cs
@@ -9,9 +9,40 @@ namespace CityLibaraySystem.Extensions
 {
     public static class StringExtensions
     {
-        public  static  string NormalizedId(this string Id)
+        public static string NormalizedId(this string Id)
         {
             return Id?.Trim().ToUpperInvariant() ?? string.Empty;
         }
+
+        public static bool IsValidEmail(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return false;
+
+            bool HasDot = false;
+            bool HasAt = false;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] == '.') { HasDot = true; }
+                if (value[i] == '@')
+                {
+                    HasAt = true;
+                }
+            }
+            return HasDot & HasAt;
+        }
+        public static bool ContainsDigit(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsDigit(value[i]))
+                    return true;
+            }
+            return false;
+        }
     }
+
 }
+    
