dba8fc66ef1b4c6058333ee90ed6fb1feeacf477
jasmine <jasminemohamed174005@gmail.com>
2026-03-11 19:58:13 +0200

    Create two interfaces (IDisplayable, IBorrowable)to code to interface rather than implementation.

---
+++ CityLibaraySystem/Extensions/IBorrowable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityLibaraySystem.Extensions
+{
+    //<summary>
+    // create a contract for the borrow capility so it will be used for any product 
+    // not just book but if we add [ magzines, newspapers, ....] theses methods will behave different
+   // </summary>
+    internal interface IBorrowable
+    {
+        void Borrow(Member member, int loanDays = 14);
+        bool IsAvailable();
+        decimal Return();
+    }
+}
---
+++ CityLibaraySystem/Extensions/IDisplayable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityLibaraySystem.Extensions
+{ 
+    //<summary>
+    // Add this interface as every class print in its own style to apply 
+    // the principle code to interface rather than implementation
+    //</summary>
+
+
+    internal interface IDisplayable
+    {
+        string ToDisplayString();
+    }
+}
