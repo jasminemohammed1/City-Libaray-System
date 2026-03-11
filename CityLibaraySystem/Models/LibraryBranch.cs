45bdd3c863e6ee1b0f695d2b0331bd12a931365f
jasmine <jasminemohamed174005@gmail.com>
2026-03-11 21:55:27 +0200

    Complete LibraryBranch class

--- CityLibaraySystem/Models/LibraryBranch.cs
+++ CityLibaraySystem/Models/LibraryBranch.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CityLibaraySystem.Extensions;
+using CityLibaraySystem.Models.Enums;
 
 namespace CityLibaraySystem.Models
 {
-    public class LibraryBranch
+    public class LibraryBranch : IDisplayable
     {
         public string BranchId { get; private set; } = null!;
         public string BranchName { get; private set; } = null!;
@@ -60,10 +62,64 @@ namespace CityLibaraySystem.Models
             return member;
         }
 
+        public Member FindMember(string MemberShipId)
+        {
+            string normalize = MemberShipId.NormalizedId();
+            for (int i = 0; i < _members.Count; i++)
+            {
+                if (_members[i].MembershipId== normalize)
+                {
+                    return _members[i];
+                }
+               
+            }
+            throw new InvalidOperationException("Member not found.");
+        }
+
+        public BookCopy FindCopy(string copyId)
+        {
+            string normalized = copyId.NormalizedId();
+            for (int i = 0; i < _copies.Count; i++)
+            {
+                if (_copies[i].CopyId == normalized)
+                    return _copies[i];
+            }
+            throw new InvalidOperationException("Book copy not found.");
+        }
+
+        public void AddBookCopy(BookCopy copy)
+        {
+            _copies.Add(copy);
+        }
+
+        public List<BookCopy> GetAvailableBookCopyies()
+        {
+            List<BookCopy> AvailList = new List<BookCopy>();
+            for (int i = 0; i < _copies.Count; i++)
+            {
+                if (_copies[i].Status == BookCopyStatus.Available)
+                {
+                    AvailList.Add(_copies[i]);
+                }
+            }
+            return AvailList;
+        }
 
 
-
-
-
+        public string ToDisplayString() => $@"ID : {BranchId}
+Name : {BranchName}
+Address : {Address}
+Phone : {Phone}
+Opening Hours : {OpeningHours}
+Manager : {Manager.Name}
+Total Members : {_members.Count}
+Total Book Copies : {_copies.Count}";
     }
+
+
+
+
+
+
 }
+
