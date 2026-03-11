c288d311d1421ee1ab269183b8fceff113ebbd6f
jasmine <jasminemohamed174005@gmail.com>
2026-03-11 20:03:30 +0200

    Create the Base Class for the Library Users(members, librarian).

---
+++ CityLibaraySystem/Models/LibraryUser.cs
@@ -0,0 +1,31 @@
+﻿using CityLibaraySystem.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityLibaraySystem.Models
+{
+    //<summary>
+    // Base class for the Member and Librarian.
+    //</summary>
+    public abstract class LibraryUser : IDisplayable
+    {
+
+        public string Name { get; protected set; }
+        public string Phone { get; protected set; }
+        public LibraryUser(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+
+
+
+        public abstract string ToDisplayString();
+        
+            
+        
+    }
+}
