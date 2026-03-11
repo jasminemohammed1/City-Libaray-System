a9619ed8ffac7befc425d3cda6877d9126b2f036
jasmine <jasminemohamed174005@gmail.com>
2026-03-11 21:48:26 +0200

    implement extension method to use it as member function

---
+++ CityLibaraySystem/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityLibaraySystem.Extensions
+{
+    public static class StringExtensions
+    {
+        public  static  string NormalizedId(this string Id)
+        {
+            return Id?.Trim().ToUpperInvariant() ?? string.Empty;
+        }
+    }
+}
