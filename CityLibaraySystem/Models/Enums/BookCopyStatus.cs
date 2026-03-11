997e7139f8df18b61d891979957b29dd5e5c3814
jasmine <jasminemohamed174005@gmail.com>
2026-03-11 19:35:05 +0200

    Divide the project into modules and create the first Eum

---
+++ CityLibaraySystem/Models/Enums/BookCopyStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityLibaraySystem.Models.Enums
+{
+    internal enum BookCopyStatus
+    {
+        Available,
+        Borrowed,
+        Damaged,
+        Reserved
+    }
+}
--- CityLibaraySystem/Program.cs
+++ CityLibaraySystem/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace CityLibaraySystem
+﻿
+namespace CityLibaraySystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
         }
     }
 }
